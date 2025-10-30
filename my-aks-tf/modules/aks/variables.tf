3d59168232dfdcf744d286cd72b3d8e4235fba89
Hoang Vo Huy <hoang.vohuy@duluxgroup.com>
2025-10-30 11:11:48 +0700

    change size

--- my-aks-tf/modules/aks/variables.tf
+++ my-aks-tf/modules/aks/variables.tf
@@ -65,7 +65,7 @@ variable "nodepools" {
     worker = {
       name                  = "worker"
       zones                 = [1, 2, 3]
-      vm_size               = "Standard_D2_v2"
+      vm_size               = "Standard_B2s"
       min_count             = 1
       max_count             = 2 
       enable_auto_scaling   = true
